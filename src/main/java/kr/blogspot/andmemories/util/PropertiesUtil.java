5c3b3f27605446c5b4b5044bf4e9d93d51baeb12
k <jhkim@aimtog.co.kr>
2016-02-01 23:17:18 +0900

    apply logback reload after base dir set

--- src/main/java/kr/blogspot/andmemories/util/PropertiesUtil.java
+++ src/main/java/kr/blogspot/andmemories/util/PropertiesUtil.java
@@ -1,13 +1,20 @@
 package kr.blogspot.andmemories.util;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 /**
@@ -34,6 +41,7 @@ public class PropertiesUtil {
 
     private static void loadLogbackConfiguration() {
         System.setProperty("logback.configurationFile", confDir+File.separator+LogbackConfig);
+        reloadLogger();
     }
 
     public static String getJMeterPropertiesFile() { return confDir+File.separator+JmeterProperties; }
@@ -48,12 +56,29 @@ public class PropertiesUtil {
      */
     private static void setDirs() throws IOException {
         if (baseDir == null) baseDir = ".";
-        confDir = baseDir+File.separator+File.separator+"conf";
-        binDir = baseDir+File.separator+File.separator+"bin";
+        confDir = baseDir+File.separator+"conf";
+        binDir = baseDir+File.separator+"bin";
 
         loadProperties();
         if (Boolean.valueOf(properties.getProperty("logback.use"))) loadLogbackConfiguration();
 
+    }
+
+    public static void reloadLogger() {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        ContextInitializer ci = new ContextInitializer(loggerContext);
+        URL url = ci.findURLOfDefaultConfigurationFile(true);
+
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(loggerContext);
+            loggerContext.reset();
+            configurator.doConfigure(url);
+        } catch (JoranException je) {
+            // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(loggerContext);
     }
 
     public static void setDirs(@NonNull String bd) throws IOException {
